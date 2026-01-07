04ba9ecc4bae127c72d6787db4a9fb045b6910d9
Yunke Qu <37522093+qykcq@users.noreply.github.com>
2026-01-07 17:48:17 +0800

    Update python_splitters.py

--- dataset_util/python_splitters.py
+++ dataset_util/python_splitters.py
@@ -5,7 +5,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split as sk_split
 import Configurations as config
 
-from model.dataset_util.split_utils import (
+from dataset_util.split_utils import (
     process_split_ratio,
     min_rating_filter_pandas,
     split_pandas_data_with_ratios,
