879a6d22ba22a7196d1f0fb305ea688f4d2815f9
Yunke Qu <37522093+qykcq@users.noreply.github.com>
2026-01-07 20:53:31 +0800

    Update NGCF.py

--- base_models/NGCF.py
+++ base_models/NGCF.py
@@ -2,7 +2,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import model.Configurations as config
+import Configurations as config
 
 
 class NGCF(nn.Module):
